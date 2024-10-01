14de6ef31f12f1b81dda9ffdf87bea444572ee3a
Sridhar1030 <sridharpillai75@gmail.com>
2024-10-01 11:57:50 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa'], // Add this line to externalize the react-icons/fa module
-    },
-  },
+	plugins: [react()],
 });
