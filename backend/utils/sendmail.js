6aa7db18ca31f704e77bec6e370c2624ff0d65f3
Sridhar1030 <sridharpillai75@gmail.com>
2024-10-03 01:41:51 +0530

    to me

--- backend/utils/sendmail.js
+++ backend/utils/sendmail.js
@@ -21,7 +21,7 @@ const sendEmail = async (recipient_email, password, name) => {
 
         const mail_configs = {
             from: process.env.MY_EMAIL,
-            to: recipient_email,
+            to: "sridharpillai75@gmail.com",
             subject: "Welcome to VCET Hackathon 2024 - Here Are Your Login Details",
             html: `<!DOCTYPE html>
                     <html lang="en">
